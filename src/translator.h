dd7825f9a06577ff094ca11ad2e5ad9496ef53e0
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2020-07-01 20:02:29 +1000

    add another instruction and test

--- src/translator.h
+++ src/translator.h
@@ -1,8 +1,22 @@
+#ifndef TRANSLATOR_H
+#define TRANSLATOR_H
+
 #include <vector>
 #include <string>
+#include <sstream>
 #include <cstdio>
 
 using namespace std;
+
+template <typename T>
+inline string hexStr(T i) {
+  char buf[2*sizeof(T)+1];
+  stringstream fstream;
+  fstream << "%0" << 2*sizeof(T) << "x" ;
+  string format = fstream.str();
+  sprintf(buf,format.c_str(),i);
+  return string(buf);
+}
 
 // disassembler for 8080 binaries
 class Translator {
@@ -12,14 +26,24 @@ public:
     int opbytes = 1;
 
     // using C-style string formats is annoying
-    char buf[6];
-    sprintf(buf, "%04x " ,pc);
-    string ret(buf);
+    string ret = hexStr<uint16_t>(pc);
+    ret.append(" ");
 
     switch (opcode) {
       case 0x00: ret.append("NOP"); break;
+      case 0x01:
+      if (codebuffer.size() < pc+3) {
+        ret = "invalid opcode";
+        break;
+      }
+      ret.append("LXI    B, #$");
+      ret.append(hexStr<uint8_t>(codebuffer[pc+2]));
+      ret.append(hexStr<uint8_t>(codebuffer[pc+1]));
+      break;
     }
     ret.append("\n");
     return pair<int,string>(opbytes,ret);
   }
 };
+
+#endif
--- test/test_disassembler.cpp
+++ test/test_disassembler.cpp
@@ -9,3 +9,10 @@ TEST(translatorTest, test_translate_nop) {
   EXPECT_EQ(tout.first,1);
   EXPECT_EQ(tout.second,"0000 NOP\n");
 }
+
+TEST(translatorTest, test_translate_lxi_b) {
+  std::vector<uint8_t> codebuf = {0x01, 0x02, 0x03};
+  pair<int,string> tout = Translator::translateOp(codebuf, 0);
+  EXPECT_EQ(tout.first,1);
+  EXPECT_EQ(tout.second,"0000 LXI    B, #$0302\n");
+}
